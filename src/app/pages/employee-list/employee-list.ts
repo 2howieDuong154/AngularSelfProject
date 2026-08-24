f847ea7b990a7e5d36e08f98e383ab88a12af968
Demon <hoaim2523002@gstudent.ctu.edu.vn>
2026-08-24 11:09:46 +0700

    [developer] Add feature employee

--- src/app/pages/employee-list/employee-list.ts
+++ src/app/pages/employee-list/employee-list.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-employee-list',
   styleUrl: './employee-list.css',
   templateUrl: './employee-list.html',
 })
-export class EmployeeList {}
+export class EmployeeList {
+  router: any = inject(Router);
+}
--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { IUser } from '../../core/model/Interfaces/User.Model';
+import { GlobalConstants } from '../../core/constants/global.constant';
 
 @Component({
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   selector: 'app-layout',
   styleUrl: './layout.css',
   templateUrl: './layout.html',
 })
-export class Layout {}
+export class Layout {
+loggedUserData!: IUser;
+router: any = inject(Router);
+
+constructor() {
+  const userData = localStorage.getItem(GlobalConstants.LOGIN_LOCAL_KEY);
+  if (userData) {
+    this.loggedUserData = JSON.parse(userData);
+  }
+}
+onLogout() {
+  localStorage.removeItem(GlobalConstants.LOGIN_LOCAL_KEY);
+  this.router.navigateByUrl('login');
+}
+
+}
